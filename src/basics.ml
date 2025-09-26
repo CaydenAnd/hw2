b47cd4151e0529f78457422453e77bd937161f92
CaydenAnd <cand164@lsu.edu>
2025-09-25 20:14:48 -0500

    attempt 1

--- src/basics.ml
+++ src/basics.ml
@@ -6,28 +6,56 @@ let abs x =
 (* Part 1: Non-Recursive Functions *)
 (***********************************)
 
-let rev_tup (tup : 'a * 'b) = failwith "unimplemented"
-let rev_triple (tup : 'a * 'b * 'c) = failwith "unimplemented"
+let rev_tup tup =
+  match tup with
+  | (a, b) -> (b, a)
 
-let is_odd x = failwith "unimplemented"
+let rev_triple tup =
+  match tup with
+  | (a, b, c) -> (c, b, a)
 
-let is_older (date1: int * int * int) (date2: int * int * int) = failwith "unimplemented"
+let is_odd x =
+  x mod 2 <> 0
 
-let to_us_format (date1: int * int * int) = failwith "unimplemented"
-(*******************************)
-(* Part 2: Recursive Functions *)
-(*******************************)
+let is_older date1 date2 =
+  let (y1, m1, d1) = date1 in
+  let (y2, m2, d2) = date2 in
+  if y1 <> y2 then y1 < y2
+  else if m1 <> m2 then m1 < m2
+  else d1 < d2
 
-let rec pow x p = failwith "unimplemented"
+let to_us_format date1 =
+  let (y, m, d) = date1 in
+  (m, d, y)
 
-let rec fac n = failwith "unimplemented"
+(************************)
+(* Part 2: Recursion    *)
+(************************)
+
+let rec pow x p =
+  if p = 0 then 1
+  else x * pow x (p - 1)
+
+let rec fac n =
+  if n = 0 then 1
+  else n * fac (n - 1)
 
 (*****************)
 (* Part 3: Lists *)
 (*****************)
 
-let rec get_nth ((idx:int), (lst: 'a list)) = failwith "unimplemented"
+let rec get_nth (idx, lst) =
+  match (idx, lst) with
+  | (0, x :: _) -> x
+  | (k, _ :: xs) when k > 0 -> get_nth (k - 1, xs)
+  | _ -> failwith "get_nth: index out of bounds"
 
-let larger lst1 lst2 = failwith "unimplemented"
+let larger lst1 lst2 =
+  let len1 = List.length lst1 in
+  let len2 = List.length lst2 in
+  if len1 > len2 then lst1
+  else if len2 > len1 then lst2
+  else []
 
-let sum lst1 lst2 = failwith "unimplemented"
+let sum lst1 lst2 =
+  List.fold_left ( + ) 0 lst1 + List.fold_left ( + ) 0 lst2
--- test/student/student.ml
+++ test/student/student.ml
@@ -1,12 +1,31 @@
 open OUnit2
 open Basics
 
-let test_sanity _ =
-    assert_equal 1 1 ~msg:"Custom error message"
+let test_sanity _ = assert_equal 1 1
+
+let test_rev_triple _ = assert_equal ('c','b','a') (rev_triple ('a','b','c'))
+
+let test_is_odd_negative _ =
+  assert_equal true (is_odd (-3));
+  assert_equal false (is_odd (-4))
+
+let test_is_older_equal _ =
+  assert_bool "" (not (is_older (2025,9,25) (2025,9,25)))
+
+let test_pow_zero _ = assert_equal 1 (pow 7 0)
+
+let test_get_nth_oob _ =
+  assert_raises (Failure "get_nth: index out of bounds")
+    (fun () -> ignore (get_nth (5,[1;2])))
 
 let suite =
   "student" >::: [
-    "sanity" >:: test_sanity
+    "sanity" >:: test_sanity;
+    "rev_triple" >:: test_rev_triple;
+    "is_odd_negative" >:: test_is_odd_negative;
+    "is_older_equal" >:: test_is_older_equal;
+    "pow_zero" >:: test_pow_zero;
+    "get_nth_oob" >:: test_get_nth_oob;
   ]
 
-let _ = run_test_tt_main suite
+let () = run_test_tt_main suite
